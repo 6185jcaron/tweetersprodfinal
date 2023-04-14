cce54eb7216ea91e23bc28fbfe6f1cb007eeda14
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-04-14 10:30:51 -0700

    bird

---
+++ birds/Assets/Scripts/Monster.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+    bool _hasDied;
+
+
+
+    
+
+    private void OnMouseDown()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    IEnumerator Start()
+    {
+        
+        {
+            float delay = UnityEngine.Random.Range(5, 30);
+            yield return new WaitForSeconds(delay);
+            if (_hasDied == false)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (ShouldDieFromCollision(collision))
+        {
+
+            StartCoroutine(ResetAfterDelay1());
+
+        }
+
+    }
+
+    bool ShouldDieFromCollision(Collision2D collision)
+    {
+        if (_hasDied)
+            return false;
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+            return true;
+
+        if (collision.contacts[0].normal.y < -0.5)
+            return true;
+
+        return false;
+    }
+
+
+    private IEnumerator ResetAfterDelay1()
+    {
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+        
+    }
+    
+}
